6c7bbc752da9f1dbb0ed4b3f31a2adb1d315b73b
santiagoamayalpz <122042569@upq.edu.mx>
2024-10-11 20:21:27 -0600

    4

---
+++ introlaravel/app/Http/Controllers/controladorVistas.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controladorVistas extends Controller
+{
+    public function home(){
+        return view('inicio');
+    }
+
+    public function insert(){
+        return view('formulario');
+    }
+
+    public function select(){
+        return view('clientes'); 
+    }
+}
