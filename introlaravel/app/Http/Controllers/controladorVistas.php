8a3715ef4730c66e47c172a5eab3e3a9fb33c713
santiagoamayalpz <122042569@upq.edu.mx>
2024-10-25 19:40:40 -0600

    Practica6

--- introlaravel/app/Http/Controllers/controladorVistas.php
+++ introlaravel/app/Http/Controllers/controladorVistas.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\validadorCliente;
 
 class controladorVistas extends Controller
 {
@@ -18,14 +19,7 @@ class controladorVistas extends Controller
         return view('clientes'); 
     }
 
-    public function procesaCliente(Request $peticion){
-
-        $validacion= $peticion->validate([ 
-            'txtnombre'=> 'required|min:3|max:20',
-            'txtapellido'=> 'required',
-            'txtcorreo'=> 'required|email',
-            'txttelefono'=> 'required|numeric',
-        ]);
+    public function procesaCliente(validadorCliente $peticion){
         
         /* redireccion usando la ruta */
         //return redirect('/');
