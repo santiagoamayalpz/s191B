b1ce540117d0762b1a9c8bffe513924433fa33fc
santiagoamayalpz <122042569@upq.edu.mx>
2024-10-31 12:39:37 -0600

    Rutas

--- biblioteca/routes/web.php
+++ biblioteca/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/', function () { return view('inicio'); })->name('inicio');
+
+Route::get('/registro', function () { return view('registro'); })->name('registro');
